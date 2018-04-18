a58da3a324f07964d08d6022d9b285ac9cd46773
Artur Minchukov <arturminchukov@gmail.com>
2018-04-18 23:49:24 +0300

    Changed travis.yml added gemini

---
+++ .gemini.js
@@ -0,0 +1,12 @@
+module.exports = {
+    rootUrl: 'http://yandex.ru',
+    gridUrl: 'http://127.0.0.1:3000',
+
+    browsers: {
+        chrome: {
+            desiredCapabilities: {
+                browserName: 'chrome'
+            }
+        }
+    }
+};
