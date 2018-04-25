ce5e852e0bc4ed07d687944fb43ae45057ce0269
Artur Minchukov <arturminchukov@gmail.com>
2018-04-25 17:07:56 +0300

    Added jest tests for Preview

---
+++ src/components/Preview/Preview.spec.js
@@ -0,0 +1,41 @@
+import  React from 'react';
+import { configure, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store'
+
+import { Preview } from './Preview';
+configure({ adapter: new Adapter });
+
+describe('<Preview />', () => {
+    const initialState = {
+        preview: {
+            picture_id: 0,
+        },
+        pictures: {
+            pictures: [{
+                images: {
+                    ['480w_still']:{
+                        width:400,
+                        height:282,
+                        url : 'https://www.decomarket24.cz/out/pictures/1/1870-1-embankment_.jpg',
+                    }
+                }
+            }],
+            number:1,
+        }
+    };
+    const mockStore = configureStore();
+    let store, wrapper;
+
+    beforeEach(() => {
+        store = mockStore(initialState);
+        wrapper = render(<Provider store={store}><Preview/></Provider>);
+    });
+
+    it('render preview', () => {
+        expect(wrapper.find('img')[0].attribs.src).toMatch(initialState.pictures.pictures[0].images['480w_still'].url);
+        expect(wrapper.find('button').length).toEqual(3);
+        expect(wrapper[0].attribs.class).toMatch('Overlay');
+    });
+});
